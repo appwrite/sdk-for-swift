9e60358d177c1f4a5692528d5705f729cd89088d
root <chiragaggarwal5k@gmail.com>
2026-03-25 06:45:10 +0000

    chore: update Swift SDK to 16.0.0

--- Sources/AppwriteEnums/Model.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public enum Model: String, CustomStringConvertible {
-    case embeddinggemma = "embeddinggemma"
-
-    public var description: String {
-        return rawValue
-    }
-}
--- Sources/AppwriteEnums/VCSReferenceType.swift
+++ Sources/AppwriteEnums/VCSReferenceType.swift
@@ -3,7 +3,6 @@ import Foundation
 public enum VCSReferenceType: String, CustomStringConvertible {
     case branch = "branch"
     case commit = "commit"
-    case tag = "tag"
 
     public var description: String {
         return rawValue
